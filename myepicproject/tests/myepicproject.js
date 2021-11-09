6ebf6dcf4d85c417435bd8571237cacd39d0b316
Kevin Kuang <200sxmeda@gmail.com>
2021-11-09 20:24:12 +1100

    prepare for devnet deploy

--- myepicproject/tests/myepicproject.js
+++ myepicproject/tests/myepicproject.js
@@ -1,13 +1,43 @@
 const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
 
 const main = async () => {
   console.log("🚀 Starting test...");
 
-  anchor.setProvider(anchor.Provider.env());
-  const program = anchor.workspace.Myepicproject;
-  const tx = await program.rpc.startStuffOff();
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
+  const program = anchor.workspace.Myepicproject;
+  const baseAccount = anchor.web3.Keypair.generate();
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
   console.log("📝 Your transaction signature", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+
+  // You'll need to now pass a GIF link to the function!
+  await program.rpc.addGif(
+    "https://data.whicdn.com/images/344494446/original.gif",
+    {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    }
+  );
+
+  // Call the account.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+
+  // Access gif_list on the account!
+  console.log("👀 GIF List", account.gifList);
 };
 
 const runMain = async () => {
